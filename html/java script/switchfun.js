2e6484d4a56da9606944f7ee8ae579d1b1ec1326
uma153 <125196110+uma153@users.noreply.github.com>
2023-02-12 23:44:51 +0530

    Add files via upload

---
+++ html/java script/switchfun.js
@@ -0,0 +1,23 @@
+function Dayweek()
+{
+    var val;
+    val=parseInt(document.getElementById("Wday").value);
+ switch(val)
+ {
+    case 1: document.write("Monday is the First day");
+       break;
+    case 2: document.write("Tuesday is the Second day");
+       break;
+    case 3: document.write("Wednesday is the Third day");
+       break;
+    case 4: document.write("Thursday is the Forth day");
+       break;
+    case 5: document.write("Friday is the Fifth day");
+       break;
+    case 6: document.write("Saturday is the Sixth day");
+       break;
+    case 7: document.write("Sunday is the Seventh day");
+       break;
+    default: document.write("invalid input");
+ }
+}
